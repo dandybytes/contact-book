4f2a8505b1e707cb22984f29ceaf68dddb01189b
dandybytes <dandybytes@gmx.com>
2019-10-15 12:33:33 +0300

    add style to active NavLinks in header/navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,45 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import Logo from "./Logo";
 import "./Header.css";
 
 const Header = () => {
-  return (
-    <header>
-      <Logo />
-      <nav className="navbar">
-        <ul className="navbar-list">
-          <li className="navbar-list-item">
-            <NavLink className="navbar-list-item-link" to="/contacts">
-              Home
-            </NavLink>
-          </li>
-          <li className="navbar-list-item">
-            <NavLink className="navbar-list-item-link" to="/add">
-              Add
-            </NavLink>
-          </li>
-          <li className="navbar-list-item">
-            <NavLink className="navbar-list-item-link" to="/about">
-              About
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <header>
+            <Logo />
+            <nav className="navbar">
+                <ul className="navbar-list">
+                    <li className="navbar-list-item">
+                        <NavLink
+                            className="navbar-list-item-link"
+                            to="/contacts"
+                            activeClassName="navbar-list-item-link-active"
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="navbar-list-item">
+                        <NavLink
+                            className="navbar-list-item-link"
+                            to="/add"
+                            activeClassName="navbar-list-item-link-active"
+                        >
+                            Add
+                        </NavLink>
+                    </li>
+                    <li className="navbar-list-item">
+                        <NavLink
+                            className="navbar-list-item-link"
+                            to="/about"
+                            activeClassName="navbar-list-item-link-active"
+                        >
+                            About
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
 };
 
 export default Header;
