6dcf5bea8fa1ef38e799a0dc3666569cd916fd60
dandybytes <dandybytes@gmx.com>
2019-10-15 14:05:08 +0300

    create pagination component

---
+++ src/components/Pagination.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Pagination.css";
+
+const Pagination = () => {
+    return (
+        <div className="Pagination">
+            <p>pagination</p>
+        </div>
+    );
+};
+
+export default Pagination;
