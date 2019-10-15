599106c8049f94a7c41d39f08c0e40e7a86c830a
dandybytes <dandybytes@gmx.com>
2019-10-15 20:20:14 +0300

    finish page-setting functionality in Pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,7 +1,9 @@
 import React from "react";
+import {connect} from "react-redux";
+import {setCurrentPage} from "../actions/actionsPagination";
 import "./Pagination.css";
 
-const Pagination = ({currentPage, itemsPerPage, totalItems}) => {
+const Pagination = ({currentPage, itemsPerPage, totalItems, setCurrentPage}) => {
     // round number of pages up
     const numPages = Math.ceil(totalItems / itemsPerPage);
     const pages = Array.from({length: numPages}, (v, i) => ++i);
@@ -10,7 +12,13 @@ const Pagination = ({currentPage, itemsPerPage, totalItems}) => {
         <nav className="pagination" aria-label="Page navigation">
             <ul className="pagination-list">
                 <li className="pagination-list-item">
-                    <a className="page-link" aria-label="previous page">
+                    <a
+                        onClick={() =>
+                            setCurrentPage(currentPage > 1 ? currentPage - 1 : currentPage)
+                        }
+                        className="page-link"
+                        aria-label="previous page"
+                    >
                         <span aria-hidden="true">«</span>
                     </a>
                 </li>
@@ -20,12 +28,24 @@ const Pagination = ({currentPage, itemsPerPage, totalItems}) => {
                         className={`pagination-list-item${currentPage === page ? " active" : ""}`}
                         key={ind}
                     >
-                        <a className="page-link">{page}</a>
+                        <a onClick={() => setCurrentPage(page)} className="page-link">
+                            {page}
+                        </a>
                     </li>
                 ))}
 
                 <li className="pagination-list-item">
-                    <a className="page-link" aria-label="next page">
+                    <a
+                        onClick={() =>
+                            setCurrentPage(
+                                totalItems > currentPage * itemsPerPage
+                                    ? currentPage + 1
+                                    : currentPage
+                            )
+                        }
+                        className="page-link"
+                        aria-label="next page"
+                    >
                         <span aria-hidden="true">»</span>
                     </a>
                 </li>
@@ -34,4 +54,4 @@ const Pagination = ({currentPage, itemsPerPage, totalItems}) => {
     );
 };
 
-export default Pagination;
+export default connect(null, {setCurrentPage})(Pagination); //prettier-ignore
