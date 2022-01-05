7fe799f63c794eb63eb3b2a1e9e995d893038347
dandybytes <dandybytes@gmx.com>
2022-01-05 22:15:46 +0100

    Update not-found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import "./NotFound.css";
+
+import Button from './common/Button'
+
 const NotFound = () => {
   return (
-    <div>
-      <p>NotFound page</p>
+    <div className="not-found-container">
+      <div className="not-found-text">
+        <p className="error-status">Error 404</p>
+        <p className="not-found-message">Page not found</p>
+      </div>
+
+      <Button href={"/contacts"} className="button-animated-borders">
+          Return to home page
+      </Button>
     </div>
   );
 };
